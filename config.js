7fdc3675eb1d0f97f0c3bba51c91682d785c0efe
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-07-08 18:42:12 -0600

    my first commit

---
+++ config.js
@@ -0,0 +1,68 @@
+export const eventData = {
+    couple: {
+      names: "Brenda & Francisco",
+      date: "26 . 02 . 2026",
+      heroImage: "/images/pareja.png"
+    },
+    song: {
+      title: "Que suerte tenerte - Fonseca",
+      file: "/Fonseca - Que Suerte Tenerte - Letra (1) (online-audio-converter.com).mp3"
+    },
+    ceremony: {
+      ceremonyImage:"/images/ceremonia.png",
+      place: "Hotel Emporio Cancún",
+      address: "Blvd. Kukulcán, Retorno Gucumatz Km 17, Segunda Etapa, Zona Hotelera, Cancún, Q.R., México",
+      date: "26 de Febrero",
+      time: "16:30 hrs",
+      mapLink: "https://maps.app.goo.gl/EuDBJDjDVKTorNGE6"
+    },
+    reception: {
+      receptionImage:"/images/reception.png",
+      place: "Hotel Emporio Cancún",
+      address: "Terraza principal",
+      date: "26 de Febrero",
+      time: "17:30 hrs",
+      mapLink: "https://maps.app.goo.gl/EuDBJDjDVKTorNGE6"
+    },
+    banks: [
+      {
+        bank: "Banco Del Universo",
+        accountName: "Isa & Roberto",
+        accountNumber: "0112233-444",
+        accountType: "Monetaria"
+      },
+      {
+        bank: "Banco Del Universo",
+        accountName: "Isa & Roberto",
+        accountNumber: "0112233-444",
+        accountType: "Monetaria"
+      }
+    ],
+    abroadGiftMessage: "Lo más importante es tu presencia, pero si deseas hacernos un regalo aquí tienes nuestros datos. ¡Muchas gracias!",
+    giftImage: "/images/gift.png",
+    
+    dresscode: {
+      description: "Hemos dejado estos links de inspiración para que puedas elegir tu atuendo. Reservado queda el color blanco e ivory para la novia.",
+      image: "/images/dresscode.png",
+      inspiration: {
+        women: "https://www.pinterest.com/search/pins/?q=Vestidos%20invitadas%20de%20boda%20de%20día",
+        men: "https://www.pinterest.com/search/pins/?q=Traje%20formal%20de%20hombres%20invitados%20a%20boda"
+      }
+    },  
+
+    finalPhoto: "/images/10.png",
+    finalMessage: "Acompáñanos en el inicio de nuestro Felices Para Siempre",
+    rsvp: {
+      rsvpImage: "/images/rsvp.png",
+      form: "https://forms.gle/F5ZgUEJgVDZU89ZSA",
+    },
+    footer: {
+      social: {
+        whatsapp: "https://wa.me/50236011737",
+        facebook: "https://www.facebook.com/thetwodesign",
+        instagram: "https://www.instagram.com/thetwodesign"
+      },
+      logo: "/images/logo.png",
+    }
+  };
+  
